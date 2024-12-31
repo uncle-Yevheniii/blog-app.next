19479c345124d3182f7455d3c65a979b776c06c7
Yevhenii <yevhenii.katsimon@gmail.com>
2024-12-31 20:41:26 +0200

    add ThemeProvider component and Radix UI button implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          disableTransitionOnChange
+          defaultTheme="system"
+          attribute="class"
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
